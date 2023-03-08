c9d176a9d6b74a67c7eebc697bc783b21d2a8de1
Feazy <francescocarvelli25@gmail.com>
2023-03-08 01:31:36 +0100

    animate the info section for the mobile view

--- src/components/Main/MovieInfo/index.js
+++ src/components/Main/MovieInfo/index.js
@@ -1,16 +1,29 @@
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import { Flex, Heading, Text, Icon, useMediaQuery } from '@chakra-ui/react';
 import { React, useState } from 'react';
 import Ratings from './Ratings';
-import { Icon, useMediaQuery } from '@chakra-ui/react';
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
 
 const MovieInfo = ({title, rating, rated, runTime, year, genre, cast, plot}) => {
+  const [showInfo, setShowInfo] = useState(() => {return false});
+  const [newTop, setNewTop] = useState(() => {return 0});
 
+  const [isLargerThan800] = useMediaQuery('(min-height: 800px)')
 
+  function show() {
+    setShowInfo(true);
+    setNewTop(window.innerHeight/2.5);
+  }
 
+  function unShow() {
+    setShowInfo(false);
+    setNewTop(0);
+  }
 
   return (
-    <Flex grow={1} h={{base: "calc(65vh)", lg: "auto"}} bg={{base: "#161A1D"}} position={{base: "relative", lg: "static"}} top={"-300px"} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} rowGap={{base: "8px",lg: "16px"}} p={{base: "8px", lg: "0"}}>
+    <Flex grow={1} h={{base: "calc(55vh)", lg: "auto"}} borderTopRadius={"16px"} bg={{base: "#161A1D"}} position={{base: "relative", lg: "static"}} top={-newTop} transition={"top 1s"} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} rowGap={{base: "8px",lg: "16px"}} p={{base: "0 8px", lg: "0"}}>
+      <Flex display={{base: "flex", lg: "none"}} position={"sticky"} bg={{base: "#161A1D"}} top={0} w={"100%"} justify={"center"} m={"0 auto"} borderBottom={"solid 1px"} cursor="pointer" onClick={ showInfo ? unShow : show}>
+        <Icon as={showInfo ? ChevronDownIcon : ChevronUpIcon} boxSize={10} />
+      </Flex>
       <Heading size={'2xl'}>{title}</Heading>
       <Ratings rating={rating}></Ratings>
       <Flex justify={"space-between"} fontSize={"20px"} fontWeight={100} >
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Spinner } from '@chakra-ui/react';
 import { React, useState, useEffect } from 'react';
 import MovieImage from './MovieImage';
 import MovieInfo from './MovieInfo';
@@ -19,21 +19,29 @@ const Main = () => {
     fetch(query)
     .then(response => response.json())
     .then(movieJson => {
+      let timer = setTimeout(() => {
+        setLoading(false);
+      }, 1000);
       setMovie(movieJson);
-      setLoading(false);
+      return () => clearTimeout(timer);
     });
   }, [query]);
-
-  if(loading) return "Loading";
 
   return (
     <Flex w={"100%"} h={"calc(100vh)"} overflowY={{base: "hidden", lg: "auto"}} justify={"center"} align={{base: "flex-start", lg: "center"}}>
       <Flex w={{base: "100%", lg: "50%"}} minH={"50%"} direction={"column"} bg={"#161A1D"} p={{base:"0", lg: "20px"}} borderRadius={{base:"0", lg: "24px"}} rowGap={{base: "0", lg: "16px"}}>
         <MovieSearch setMovieTitle={setTitle} />
-        <Flex w={"100%"} direction={{base: "column", lg: "row"}} columnGap={{base: "0", lg: "16px"}}>
-          <MovieImage source={movie.Poster} alter={movie.Title} />
-          <MovieInfo title={movie.Title} rating={movie.Ratings[0]} rated={movie.Rated} runTime={movie.Runtime} year={movie.Year} genre={movie.Genre} cast={movie.Actors} plot={movie.Plot} />
+        
+        {loading ? 
+        <Flex w={"100%"} justify={"center"} align={"center"} p={"16px"}>
+          <Spinner color='#BA181B' size='xl' /> 
         </Flex>
+        : 
+          <Flex w={"100%"} direction={{base: "column", lg: "row"}} columnGap={{base: "0", lg: "16px"}}>
+            <MovieImage source={movie.Poster} alter={movie.Title} />
+            <MovieInfo title={movie.Title} rating={movie.Ratings[0]} rated={movie.Rated} runTime={movie.Runtime} year={movie.Year} genre={movie.Genre} cast={movie.Actors} plot={movie.Plot} />
+          </Flex>
+        }
       </Flex>
     </Flex>
   )
