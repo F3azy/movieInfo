3f5943d210b3d2b00513b8122d21b7b6d6c281c0
Feazy <francescocarvelli25@gmail.com>
2023-03-09 20:03:26 +0100

    deploy works thank to adding one letter :) now adjusting the moving bar for phone view

--- src/components/Main/MovieInfo/index.js
+++ src/components/Main/MovieInfo/index.js
@@ -7,6 +7,7 @@ import SmallInfo from './SmallInfo';
 const MovieInfo = ({title, rating, rated, runTime, year, genre, cast, plot}) => {
   const [showInfo, setShowInfo] = useState(() => {return false});
   const [newBottom, setNewBottom] = useState(() => {return 4});
+  const [newHeight, setNewHeight] = useState(() => {return 90});
 
   const information = useState(() => { return [
     {
@@ -25,29 +26,33 @@ const MovieInfo = ({title, rating, rated, runTime, year, genre, cast, plot}) => 
 
   function show() {
     setShowInfo(true);
-    setNewBottom(window.innerHeight/2);
+    setNewBottom(550);
+    setNewHeight(550);
   }
 
   function unShow() {
     setShowInfo(false);
     setNewBottom(4);
+    setNewHeight(90);
   }
 
   return (
-    <Flex grow={1} h={{base: window.innerHeight/2, sm: "86vh", lg: "auto"}} borderTopRadius={"16px"} bg={{base: "#161A1D"}} position={{base: "relative", md: "static"}} bottom={{base: newBottom, sm: 0}} transition={"bottom 1s"} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} rowGap={{base: "8px", md: "16px"}} p={{base: "4px 8px", lg: "0"}}>
-      <Flex display={{base: "flex", sm: "none", lg: "none"}} position={"sticky"} bg={{base: "#161A1D"}} top={0} w={"100%"} justify={"center"} m={"0 auto"} borderBottom={"solid 1px"} cursor="pointer" onClick={ showInfo ? unShow : show}>
-        <Icon as={showInfo ? ChevronDownIcon : ChevronUpIcon} boxSize={10} />
-      </Flex>
-      <Heading size={'2xl'}>{title}</Heading>
-      <Ratings rating={rating}></Ratings>
-      <Flex justify={"space-between"} fontSize={"20px"} fontWeight={100} >
-        <Text>{rated}</Text>
-        <Text>{runTime}</Text>
-        <Text>{year}</Text>
-      </Flex>
+    <Flex grow={1} borderTopRadius={"16px"} bg={{base: "#161A1D"}} position={{base: "relative", md: "static"}} bottom={{base: newBottom, sm: 0}} transition={"bottom 1s"} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} p={{base: "0 8px", lg: "0"}}>
+        <Flex display={{base: "flex", sm: "none", lg: "none"}} position={"sticky"} bg={{base: "#161A1D"}} top={0} w={"100%"} justify={"center"} m={"0 auto"} borderBottom={"solid 1px"} cursor="pointer" onClick={ showInfo ? unShow : show}>
+          <Icon as={showInfo ? ChevronDownIcon : ChevronUpIcon} boxSize={10} />
+        </Flex>
+      <Flex h={{base: newHeight, sm: "86vh", lg: "auto"}} transition={"height 1s"} borderTopRadius={"16px"} bg={{base: "#161A1D"}} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} rowGap={{base: "8px", md: "16px"}}>
+        <Heading size={'2xl'}>{title}</Heading>
+        <Ratings rating={rating}></Ratings>
+        <Flex justify={"space-between"} fontSize={"20px"} fontWeight={100} >
+          <Text>{rated}</Text>
+          <Text>{runTime}</Text>
+          <Text>{year}</Text>
+        </Flex>
 
-      {information[0]?.map((info) => <SmallInfo key={info.title} infoTitle={info.title} content={info.content} />)}
+        {information[0]?.map((info) => <SmallInfo key={info.title} infoTitle={info.title} content={info.content} />)}
 
+      </Flex>
     </Flex>
   )
 };
