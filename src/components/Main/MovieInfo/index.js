62f833a8221e6142afad057bb96262cc1dd59ceb
Feazy <francescocarvelli25@gmail.com>
2023-03-10 22:54:22 +0100

    added divider instead of borderBottom and tried to use a skeleton for loading but decided that is ok as it is for this project and will try with the next one probably no to destroy this one

--- src/components/Main/MovieInfo/index.js
+++ src/components/Main/MovieInfo/index.js
@@ -1,4 +1,4 @@
-import { Flex, Heading, Text, Icon } from '@chakra-ui/react';
+import { Flex, Heading, Text, Icon, Divider  } from '@chakra-ui/react';
 import { React, useState } from 'react';
 import Ratings from './Ratings';
 import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons';
@@ -38,9 +38,10 @@ const MovieInfo = ({title, rating, rated, runTime, year, genre, cast, plot}) => 
 
   return (
     <Flex grow={1} borderTopRadius={"16px"} bg={{base: "#161A1D"}} position={{base: "relative", md: "static"}} bottom={{base: newBottom, sm: 0}} transition={"bottom 1s"} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} p={{base: "0 8px", lg: "0"}}>
-        <Flex display={{base: "flex", sm: "none", lg: "none"}} position={"sticky"} bg={{base: "#161A1D"}} top={0} w={"100%"} justify={"center"} m={"0 auto"} borderBottom={"solid 1px"} cursor="pointer" onClick={ showInfo ? unShow : show}>
+        <Flex display={{base: "flex", sm: "none", lg: "none"}} position={"sticky"} bg={{base: "#161A1D"}} top={0} w={"100%"} justify={"center"} m={"0 auto"} cursor="pointer" onClick={ showInfo ? unShow : show}>
           <Icon as={showInfo ? ChevronDownIcon : ChevronUpIcon} boxSize={10} />
         </Flex>
+        <Divider display={{base: "flex", sm: "none", lg: "none"}}  />
       <Flex h={{base: newHeight, sm: "86vh", lg: "auto"}} transition={"height 1s"} borderTopRadius={"16px"} bg={{base: "#161A1D"}} overflowY={{base: "scroll", lg: "auto"}} direction={"column"} rowGap={{base: "8px", md: "16px"}}>
         <Heading size={'2xl'}>{title}</Heading>
         <Ratings rating={rating}></Ratings>
