b54a0be3587f91a5247c1b80a2da47cc200bf1f6
Francesco <francescocarvelli25@gmail.com>
2023-11-09 18:42:54 +0100

    changed the way the background image is shown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from "react";
 import { theme } from "./styles/theme";
 import "./styles/globals.css";
-import { ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
 import {
   MovieImage,
   MovieInfo,
@@ -34,72 +34,70 @@ function App() {
   }, [query]);
 
   useEffect(() => {
-    if(movie && movie.Response !== "False") setBgImg(movie.Poster)
-  }, [movie])
+    if (movie && movie.Response !== "False") setBgImg(movie.Poster);
+  }, [movie]);
 
   return (
     <ChakraProvider theme={theme}>
-      <Flex
+      <Box
         bgImage={bgImg && `url(${bgImg})`}
         bgSize="cover"
         bgPosition="center"
-        position="absolute"
-        top={0}
-        bottom={0}
-        left={0}
-        right={0}
-        zIndex={0}
-      />
-      <Flex
-        w="100%"
+      >
+        <Flex 
         minH="calc(100vh)"
         justify="center"
-        align="center"
+        align="center"       
         backdropFilter={{ base: "auto" }}
         backdropBlur={{ base: "70px" }}
-        position="relative"
-        zIndex={10}
-      >
-        <Flex
-          w={{ base: "100%", lg: "90%", xl: "70%", "2xl": "50%" }}
-          m="0 auto"
-          minH={{ base: "calc(100vh)", lg: "auto" }}
-          direction="column"
-          bg={{ base: "transparent", lg: "#161A1D" }}
-          p={{ base: "0", lg: "20px" }}
-          borderRadius={{ base: "0", lg: "36px" }}
-          rowGap="20px"
         >
-          <MovieSearchInput setMovieTitle={setTitle} />
+          <Flex
+            w={{ base: "100%", lg: "90%", xl: "70%", "2xl": "50%" }}
+            m="0 auto"
+            minH={{ base: "calc(100vh)", lg: "auto" }}
+            direction="column"
+            bg={{ base: "transparent", lg: "#161A1D" }}
+            p={{ base: "0", lg: "20px" }}
+            borderRadius={{ base: "0", lg: "36px" }}
+            rowGap="20px"
+          >
+            <MovieSearchInput setMovieTitle={setTitle} />
 
-          {loading ? (
-            <Flex w="100%" minH="90%" justify="center" align="center" p="16px">
-              <Spinner color="#BA181B" size="xl" />
-            </Flex>
-          ) : notFound ? (
-            <MovieNotFound err={movie.Error} />
-          ) : (
-            <Flex
-              w="100%"
-              direction={{ base: "column", lg: "row" }}
-              columnGap={{ base: "0", lg: "16px" }}
-              rowGap={{ base: "20px", lg: "0" }}
-            >
-              <MovieImage source={movie.Poster} alter={movie.Title} />
-              <MovieInfo
-                title={movie.Title}
-                rating={movie.Ratings[0]}
-                rated={movie.Rated}
-                runTime={movie.Runtime}
-                year={movie.Year}
-                genre={movie.Genre}
-                cast={movie.Actors}
-                plot={movie.Plot}
-              />
-            </Flex>
-          )}
+            {loading ? (
+              <Flex
+                w="100%"
+                minH="90%"
+                justify="center"
+                align="center"
+                p="16px"
+              >
+                <Spinner color="#BA181B" size="xl" />
+              </Flex>
+            ) : notFound ? (
+              <MovieNotFound err={movie.Error} />
+            ) : (
+              <Flex
+                w="100%"
+                direction={{ base: "column", lg: "row" }}
+                columnGap={{ base: "0", lg: "16px" }}
+                rowGap={{ base: "20px", lg: "0" }}
+              >
+                <MovieImage source={movie.Poster} alter={movie.Title} />
+                <MovieInfo
+                  title={movie.Title}
+                  rating={movie.Ratings[0]}
+                  rated={movie.Rated}
+                  runTime={movie.Runtime}
+                  year={movie.Year}
+                  genre={movie.Genre}
+                  cast={movie.Actors}
+                  plot={movie.Plot}
+                />
+              </Flex>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
+      </Box>
     </ChakraProvider>
   );
 }
